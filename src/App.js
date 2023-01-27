bbf90b34866e457c8b9fed37c3da1b8580e422f6
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-27 15:11:56 +0100

    complete form design

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import data from './data'
 function App() {
   const [count, setCount] = useState(0)
   const [items, setItems] = useState([])
-  
-  return <h2>lorem ipsum project setup</h2>
+
+  return (
+    <div className="section-center">
+      <h3>Tired of boring Lorem Ipsum</h3>
+      <form onSubmit={handleSubmit} className='lorem-form'>
+        <label htmlFor='amount'>
+          Paragraphs: 
+        </label>
+        <input type='number' name='amount' value={count} onChange={(e) => setCount(e.target.value)}/>
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+      
+    </div>
+  )
 }
 
 export default App
