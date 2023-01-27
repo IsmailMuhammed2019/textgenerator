e5cd7b31fcb3830f4e43ecd2fa1f57215f47b3e5
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-27 15:04:51 +0100

    declared states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import data from './data'
 function App() {
+  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([])
+  
   return <h2>lorem ipsum project setup</h2>
 }
 
