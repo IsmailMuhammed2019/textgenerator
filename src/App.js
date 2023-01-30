02754c775bb5ce603367ec0177bf3fea7a46c207
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-30 11:02:17 +0100

    update app data structure

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, { useState } from 'react'
 import data from './data'
 function App() {
-  const [count, setCount] = useState(0)
-  const [items, setItems] = useState([])
+const [count, setCount] = useState(0)
+const [items,setItems] = useState([])
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    let amount = parseInt(count)
-    if(count <= 0) {
+const handleClick = (e) => {
+  e.preventDefault()
+  let amount = parseInt(count)
+
+  setItems(data.slice(0, amount))
+    if (count <= 0) {
       amount = 1
     }
-    if (count > 8) {
-      amount = items.lenght
+    if (count > items.length) {
+      amount = items.length
     }
-    setItems(data.slice(0, amount))
-  }
+
+}
+
   return (
     <div className="section-center">
-      <h3>Tired of boring Lorem Ipsum</h3>
-      <form onSubmit={handleSubmit} className='lorem-form'>
-        <label htmlFor='amount'>
-          Paragraphs: 
-        </label>
-        <input type='number' name='amount' value={count} onChange={(e) => setCount(e.target.value)}/>
-        <button type='submit' className='btn'>Generate</button>
+      <h2>Tired of boring lorem-ipusm text</h2>
+      <form onSubmit={handleClick} className="lorem-form">
+        <label htmlFor='amount' className='amount'>Paragraph</label>
+        <input type="number" name="amount" id="amount" onChange={(e) => setCount(e.target.value)} value={count} />
+        <button className="btn" type='submit'>Generate</button>
       </form>
-      <article className='lorem-text'>
-      {items.map((item, index) => {
-        return (
-          <p key={index}>{item}</p>
-        )
-      })}
+      <article className="lorem-text">
+        {items.map((item, index) => {
+          return (
+            <p key={index}>{item}</p>
+          )
+        })}
       </article>
     </div>
   )
